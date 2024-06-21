b8b766a6dd7a2d12eaaea63882c659f271bd1598
glongh <glongh@gmail.com>
2024-06-21 19:49:18 -0400

    Accelerometer, BackgroundTask and Metrics Managers

--- app/lighthar/lighthar/Manager/BackgroundTaskManager.swift
+++ app/lighthar/lighthar/Manager/BackgroundTaskManager.swift
@@ -1,8 +1,36 @@
-//
-//  BackgroundTaskManager.swift
-//  lighthar
-//
-//  Created by Gaston Longhitano on 6/21/24.
-//
+/**
+ * Gaston Longhitano <gastonl@bu.edu> @ Boston University - Research
+ *
+ * This source code is licensed under the terms found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import UIKit
 
-import Foundation
+class BackgroundTaskManager {
+    var registerAsLoop: Bool = false
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+    
+    func registerBackgroundTask() {
+        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+            self?.endBackgroundTask()
+        }
+        print("Background task started.")
+    }
+    
+    func endBackgroundTask() {
+        UIApplication.shared.endBackgroundTask(backgroundTask)
+        backgroundTask = .invalid
+        print("Background task ended.")
+        
+        if(registerAsLoop) {
+            backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+                self?.endBackgroundTask()
+            }
+            print("Background task started.")
+        }
+    }
+    
+    func setRegisterAsLoop(value: Bool){
+        registerAsLoop = value
+    }
+}
--- app/lighthar/lighthar/Manager/MetricsHandler.swift
+++ app/lighthar/lighthar/Manager/MetricsHandler.swift
@@ -1,8 +1,34 @@
-//
-//  MetricsHandler.swift
-//  lighthar
-//
-//  Created by Gaston Longhitano on 6/14/24.
-//
+/**
+ * Gaston Longhitano <gastonl@bu.edu> @ Boston University - Research
+ *
+ * This source code is licensed under the terms found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-import Foundation
+import MetricKit
+
+
+class MetricsHandler: NSObject, MXMetricManagerSubscriber {
+    static let shared = MetricsHandler()
+    
+    var onMemoryUsageUpdate: ((Double) -> Void)?
+    var onBatteryUsageUpdate: ((Double) -> Void)?
+    
+    func didReceive(_ payloads: [MXMetricPayload]) {
+        for payload in payloads {
+            if let memoryMetrics = payload.memoryMetrics {
+                let peakMemoryUsageKB = memoryMetrics.peakMemoryUsage.converted(to: .kilobytes).value
+                onMemoryUsageUpdate?(peakMemoryUsageKB)
+            }
+            if let batteryMetrics = payload.cpuMetrics {
+                let energyConsumption = estimateEnergyConsumption(cpuTimeInSeconds: batteryMetrics.cumulativeCPUTime.converted(to: .seconds).value)
+                onBatteryUsageUpdate?(energyConsumption)
+            }
+        }
+    }
+    
+    func didReceive(_ payloads: [MXDiagnosticPayload]) {
+        print(payloads)
+        // For this research we are not going to handle the received diagnostics
+    }
+}
